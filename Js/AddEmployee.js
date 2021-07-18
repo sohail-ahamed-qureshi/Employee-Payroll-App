e5155ad70b2a57e3a068b0a86b8435f9f6cb99cc
sohail <sohailqureshi82@gmail.com>
2021-07-19 03:03:48 +0530

    [sohail] Add: ability to submit form with validations

--- Js/AddEmployee.js
+++ Js/AddEmployee.js
@@ -63,15 +63,24 @@ day.addEventListener("change", validateDate);
 
 
 //profile and gender validations
+let filepath,genderValue;
 var profileImage = document.getElementsByName('profile');
 var gender = document.getElementsByName('gender');
 for(let index =0; index < profileImage.length; index++){
   profileImage[index].addEventListener("click", function(){
+    filepath=profileImage[index].value;
     if(profileImage[0].checked || profileImage[2].checked){
       gender[0].checked = true;
-    }else{
+      genderValue = gender[0].value;
+    }if(profileImage[1].checked || profileImage[3].checked){
       gender[1].checked = true;
+      genderValue = gender[1].value;
     }
   });
 }
 
+//form submit
+var submit = document.querySelector("#submitButton");
+submit.addEventListener("click", function(){
+alert(fullName.value +" "+ filepath +" "+ genderValue)
+});
